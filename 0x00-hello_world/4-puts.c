24d14d38322f73f9ba05c9c67fe792a839de0974
ZoeSamm <zoemultiply@gmail.com>
2022-09-08 13:35:06 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 /**
  * main - entry point
@@ -5,6 +6,6 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
