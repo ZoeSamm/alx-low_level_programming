88d4656d0f1455aad4898b8fe73a22a21e458fb9
ZoeSamm <zoemultiply@gmail.com>
2022-09-15 16:08:17 -0700

    header function

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -6,3 +11,5 @@ char _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+
+#endif /* MAIN_H */
