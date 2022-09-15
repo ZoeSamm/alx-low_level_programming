10f9efcf41c978aeb9d2accd62e7bf888eef07a8
ZoeSamm <zoemultiply@gmail.com>
2022-09-15 15:46:46 -0700

    main h function

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+char _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
