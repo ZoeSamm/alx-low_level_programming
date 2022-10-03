44626aa1d389958c6e3949b3c2a84c3cad621076
ZoeSamm <zoemultiply@gmail.com>
2022-10-03 14:57:19 -0700

    creating array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,6 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *s;
-
 	if (size == 0)
 		return (NULL);
 	s = (char *)malloc(size * sizeof(char));
@@ -18,7 +17,5 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = c;
-
 	return (s);
 }
-
