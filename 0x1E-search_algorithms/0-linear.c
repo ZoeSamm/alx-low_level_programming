200e8a60d3f9965d78bf1a841d56e9da84eb6ce1
ZoeSamm <zoemultiply@gmail.com>
2023-04-12 03:17:26 -0700

    algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,15 +1,9 @@
-#include <stdio.h>
-
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
-    for (size_t i = 0; i < size; i++) {
-        printf("Comparing value %d with %d\n", array[i], value);
-        if (array[i] == value) {
-            return i;
-        }
-    }
-    return -1;
-}
+def linear_search(array, size, value):
+    if array is None:
+        return -1
+    for i in range(size):
+        print("Comparing value {} with {}".format(array[i], value))
+        if array[i] == value:
+            return i
+    return -1
 
